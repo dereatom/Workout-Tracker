3ba31e29b41f7d905c88f94b19d10ccd0df5b4c5
Dereje Ayele <derejeatomsa2@gmail.com>
2021-06-28 19:43:01 -0400

    brushing the code

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 // routes
 app.use(require("./routes/apiRoutes"));
 app.use(require("./routes/htmlRoutes"));
-// require("./routes/apiRoutes");
-// require("./routes/htmlRoutes");
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
