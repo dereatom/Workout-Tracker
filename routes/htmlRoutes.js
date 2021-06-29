e1611835bb197006bda6b903bb85fec66e72542b
Dereje Ayele <derejeatomsa2@gmail.com>
2021-06-28 21:22:45 -0400

    minor fixing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const router = require("express").Router();
 
-
   router.get("/exercise",function (req,res){   
       res.sendFile(path.join(__dirname,"../public/exercise.html"));
   });
